971d73c0eca777fa7948ef1a220643cde4052c17
toowboga <jannis.thomae@stud.hawk.de>
2022-07-24 00:35:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#remove old 
-#rm /tmp/.X99-lock #needed when docker container is restarted
-Xvfb :99 -screen 0 640x480x8 -nolisten tcp &
+# create a virtual monitor with Xvfb
+Xvfb :99 -screen 0 1600x1200x24 +extension GLX +render -noreset -nolisten tcp &
 cd /home/app
-ls -al
+# show version infos
 mvn --version
 java --version
 javac --version
+# show OpenGL version
+glxinfo | grep "OpenGL version"
+# start application with mvn javafx:run command
 mvn -f pom.xml javafx:run

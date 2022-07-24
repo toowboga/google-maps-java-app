9f35139f619333e2d839cfb78400462bcc999be9
toowboga <jannis.thomae@stud.hawk.de>
2022-07-24 23:16:59 +0200

    Update run-script.sh add docker pull command

--- run-script.sh
+++ run-script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# pull image from docker hub
+docker pull toowboga/google-maps-java-app:latest
+
 # allow access from localhost
 xhost + 127.0.0.1
 
 # run google-maps-java-app with X11 forwarding and keep running until it quits
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=host.docker.internal:0 --privileged --name google-maps-app google-maps-app:latest
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=host.docker.internal:0 --privileged --name toowboga/google-maps-app google-maps-app:latest

539c7403cceda44317fef6baa337610a400cf68c
toowboga <jannis.thomae@stud.hawk.de>
2022-07-24 05:15:34 +0200

    Update entrypoint.sh set screen size for virtual frame buffer

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # create a virtual monitor with Xvfb
-Xvfb :99 -screen 0 1600x1200x24 +extension GLX +render -noreset -nolisten tcp &
+Xvfb :99 -screen 0 1200x800x24 +extension GLX +render -noreset -nolisten tcp &
 cd /home/app
 # show version infos
 mvn --version

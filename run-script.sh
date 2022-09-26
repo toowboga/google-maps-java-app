e5df6ec3d9d844aedb29ced0916d5b26625eb688
toowboga <jannis.thomae@stud.hawk.de>
2022-09-26 22:06:53 +0200

    Update run-script.sh

--- run-script.sh
+++ run-script.sh
@@ -7,4 +7,4 @@ docker pull toowboga/google-maps-java-app:latest
 xhost + 127.0.0.1
 
 # run google-maps-java-app with X11 forwarding and keep running until it quits
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=host.docker.internal:0 --privileged --name google-maps-app toowboga/google-maps-java-app:latest
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix:rw -e DISPLAY=host.docker.internal:0 --name google-maps-app toowboga/google-maps-java-app:latest
